00aa595a1ac0cbe8613e136cbfdc8137ad8ac3b9
Jaysen <nesyaj13@gmail.com>
2023-08-23 05:15:41 +0800

    add constants and api for all territories

---
+++ imports/contants.js
@@ -0,0 +1,4 @@
+let LOCAL_STORAGE_USER = "user";
+let NETZWELT_BASE_URL = "https://netzwelt-devtest.azurewebsites.net";
+
+export { LOCAL_STORAGE_USER, NETZWELT_BASE_URL };
--- server/main.js
+++ server/main.js
@@ -1,16 +1,15 @@
-import { Meteor } from "meteor/meteor";
 import { WebApp } from "meteor/webapp";
 import bodyParser from "body-parser";
 import { fetch, Headers } from "meteor/fetch";
 import { processIncomingData } from "./helpers";
+import { NETZWELT_BASE_URL } from "../imports/contants";
 
 WebApp.connectHandlers.use(bodyParser.urlencoded({ extended: false }));
-WebApp.connectHandlers.use("/login", (req, res, next) => {
-  let baseUrl = "https://netzwelt-devtest.azurewebsites.net";
+WebApp.connectHandlers.use("/login", (req, res) => {
   processIncomingData(req)
     .then(async (body) => {
       try {
-        const response = await fetch(baseUrl + "/Account/SignIn", {
+        const response = await fetch(NETZWELT_BASE_URL + "/Account/SignIn", {
           method: "POST",
           headers: new Headers({
             "Content-Type": "application/json",
@@ -21,12 +20,21 @@ WebApp.connectHandlers.use("/login", (req, res, next) => {
         res.writeHead(200);
         res.end(JSON.stringify(data));
       } catch (error) {
-        res.writeHead(400);
-        res.end(JSON.stringify({ error }));
+        throw error;
       }
     })
     .catch((err) => {
       res.writeHead(400);
       res.end(JSON.stringify({ err }));
     });
+});
+WebApp.connectHandlers.use("/allTeritories", async (req, res) => {
+  try {
+    const response = await fetch(NETZWELT_BASE_URL + "/Territories/All", { method: "GET" });
+    const data = await response.json();
+    res.writeHead(200);
+    res.end(JSON.stringify(data));
+  } catch (error) {
+    throw error;
+  }
 });
